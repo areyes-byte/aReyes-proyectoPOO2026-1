d31bb797017afa186fff3dad7a5f427580169672
areyes-byte <areyes@pucp.edu.pe>
2026-04-29 02:20:39 -0500

    adding password textbox

--- MyForm.h
+++ MyForm.h
@@ -37,7 +37,11 @@ namespace aReyesProyectoPOO20261 {
 	private: System::Windows::Forms::Button^ button1;
 	protected:
 	private: System::Windows::Forms::Label^ label1;
-	private: System::Windows::Forms::TextBox^ textBox1;
+	private: System::Windows::Forms::TextBox^ textBoxUser;
+
+	private: System::Windows::Forms::Label^ label2;
+	private: System::Windows::Forms::TextBox^ textBoxPassword;
+
 
 	private:
 		/// <summary>
@@ -54,7 +58,9 @@ namespace aReyesProyectoPOO20261 {
 		{
 			this->button1 = (gcnew System::Windows::Forms::Button());
 			this->label1 = (gcnew System::Windows::Forms::Label());
-			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->textBoxUser = (gcnew System::Windows::Forms::TextBox());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->textBoxPassword = (gcnew System::Windows::Forms::TextBox());
 			this->SuspendLayout();
 			// 
 			// button1
@@ -70,26 +76,47 @@ namespace aReyesProyectoPOO20261 {
 			// label1
 			// 
 			this->label1->AutoSize = true;
-			this->label1->Location = System::Drawing::Point(189, 41);
+			this->label1->Location = System::Drawing::Point(34, 66);
 			this->label1->Name = L"label1";
-			this->label1->Size = System::Drawing::Size(43, 13);
+			this->label1->Size = System::Drawing::Size(29, 13);
 			this->label1->TabIndex = 1;
-			this->label1->Text = L"Usuario";
+			this->label1->Text = L"User";
 			// 
-			// textBox1
+			// textBoxUser
 			// 
-			this->textBox1->Location = System::Drawing::Point(238, 38);
-			this->textBox1->Name = L"textBox1";
-			this->textBox1->Size = System::Drawing::Size(100, 20);
-			this->textBox1->TabIndex = 2;
-			this->textBox1->TextChanged += gcnew System::EventHandler(this, &MyForm::textBox1_TextChanged);
+			this->textBoxUser->Location = System::Drawing::Point(69, 63);
+			this->textBoxUser->Name = L"textBoxUser";
+			this->textBoxUser->Size = System::Drawing::Size(100, 20);
+			this->textBoxUser->TabIndex = 2;
+			this->textBoxUser->TextChanged += gcnew System::EventHandler(this, &MyForm::textBox1_TextChanged);
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Location = System::Drawing::Point(12, 104);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(53, 13);
+			this->label2->TabIndex = 3;
+			this->label2->Text = L"Password";
+			this->label2->Click += gcnew System::EventHandler(this, &MyForm::label2_Click);
+			// 
+			// textBoxPassword
+			// 
+			this->textBoxPassword->Location = System::Drawing::Point(69, 104);
+			this->textBoxPassword->Name = L"textBoxPassword";
+			this->textBoxPassword->Size = System::Drawing::Size(100, 20);
+			this->textBoxPassword->TabIndex = 4;
+			this->textBoxPassword->UseSystemPasswordChar = true;
+			this->textBoxPassword->TextChanged += gcnew System::EventHandler(this, &MyForm::textBoxPassword_TextChanged);
 			// 
 			// MyForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->ClientSize = System::Drawing::Size(634, 341);
-			this->Controls->Add(this->textBox1);
+			this->Controls->Add(this->textBoxPassword);
+			this->Controls->Add(this->label2);
+			this->Controls->Add(this->textBoxUser);
 			this->Controls->Add(this->label1);
 			this->Controls->Add(this->button1);
 			this->Name = L"MyForm";
@@ -100,11 +127,15 @@ namespace aReyesProyectoPOO20261 {
 		}
 #pragma endregion
 	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) { // Evento del botón de login al presionarlo
-		String^ usuario = this->textBox1->Text; // Obtener el texto ingresado en el TextBox
-		Console::WriteLine("Usuario ingresado: "+ usuario); // Imprimir el usuario en la consola (puedes reemplazar esto con la lógica de autenticación)
+		String^ User = this->textBoxUser->Text; // Obtener el texto ingresado en el TextBox
+		Console::WriteLine("Usuario ingresado: "+ User); // Imprimir el usuario en la consola (puedes reemplazar esto con la lógica de autenticación)
 
 	}
 	private: System::Void textBox1_TextChanged(System::Object^ sender, System::EventArgs^ e) {
 	}
-	};
+	private: System::Void label2_Click(System::Object^ sender, System::EventArgs^ e) {
+	}
+private: System::Void textBoxPassword_TextChanged(System::Object^ sender, System::EventArgs^ e) {
+}
+};
 }
